3aa4c76af0ce11e229e5d5ec7835f937d280db3e
Liang Xu <liangxu@Liangs-MBP.attlocal.net>
2022-01-24 10:04:34 -0800

    add s3 mock

--- example/conftest.py
+++ example/conftest.py
@@ -1,4 +1,7 @@
+import boto3
+import os
 import pytest
+from moto import mock_s3
 from models import MyModel
 
 
@@ -24,3 +27,19 @@ def mock_my_model():
         id="my_mock_id",
     )
     return my_model
+
+
+@pytest.fixture
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+
+
+@pytest.fixture
+def s3_client(aws_credentials):
+    with mock_s3():
+        conn = boto3.client("s3", region_name="us-east-1")
+        yield conn
